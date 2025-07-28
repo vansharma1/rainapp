6ae1b2e130786db4d9deb87b495ccd320f6f08b2
vansharma1 <212800374+vansharma1@users.noreply.github.com>
2025-07-28 13:41:45 +0530

    Create mockIP.js

---
+++ src/mock/mockIP.js
@@ -0,0 +1,10 @@
+export default function mockIP() {
+  // Always returns "Mockville"
+  return {
+    lat: 28.6139,
+    lon: 77.2090,
+    city: "Mockville",
+    region: "Mockland",
+    country: "Mockistan"
+  };
+}
